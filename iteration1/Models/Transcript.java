0f13a7694f0a5ef271396a5d94a9e9b4fc1b6f95
berkkanrencber <101739059+berkkanrencber@users.noreply.github.com>
2022-11-13 17:44:48 +0300

    //I designed the transcript class.

---
+++ iteration1/Models/Transcript.java
@@ -0,0 +1,51 @@
+package iteration1.Models;
+
+public class Transcript {
+    private double gpa;
+    private int completedCredit;
+    private ArrayList<Course> completedCourses;
+    private  ArrayList<Course> failedCourses;
+
+    public void  calculateGPA(){
+        double tempGpa=0;
+        for (Course course:completedCourses){
+            double randomScore=Math.random()4;
+            tempGpa+=course.credit*randomScore;
+        }
+        this.gpa=tempGpa/completedCredit;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        if (gpa >= 0) {
+            this.gpa = gpa;
+        }
+    }
+
+    public int getCompletedCredit() {
+        return completedCredit;
+    }
+
+    public void setCompletedCredit(int completedCredit) {
+        this.completedCredit = completedCredit;
+    }
+
+    public ArrayList<Course> getCompletedCourses() {
+        return completedCourses;
+    }
+
+    public void setCompletedCourses(ArrayList<Course> completedCourses) {
+        this.completedCourses = completedCourses;
+    }
+
+    public ArrayList<Course> getFailedCourses() {
+        return failedCourses;
+    }
+
+    public void setFailedCourses(ArrayList<Course> failedCourses) {
+        this.failedCourses = failedCourses;
+    }
+}

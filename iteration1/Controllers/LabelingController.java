5f2016bff10564f7ad77204050b25a826489651a
YasinSefa15 <yasekirman@gmail.com>
2022-11-13 22:54:42 +0300

    Labeling controller improvement

--- iteration1/Controllers/LabelingController.java
+++ iteration1/Controllers/LabelingController.java
@@ -3,24 +3,39 @@ package iteration1.Controllers;
 import iteration1.Models.Advisor;
 import iteration1.Models.Course;
 import iteration1.Models.Curriculum;
+import iteration1.Models.Student;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class LabelingController extends Controller {
+    ArrayList<Student> students;
+    ArrayList<Advisor> advisors;
+
     public void execute() {
         initObjects();
+        initAdvisors();
     }
 
     public void initObjects() {
-
+        initStudents();
     }
 
     public ArrayList<Course> initCourses() {
         return null;
     }
 
-    public ArrayList<Course> initStudents() {
-        return null;
+    public ArrayList<Student> initStudents() {
+        students = new ArrayList<>();
+        ArrayList<JSONObject> objects = readJSONFiles("Students");
+        objects.forEach((n) -> students.add(new Student(
+                n.getString("id"),
+                n.getInt("registerDate"),
+                n.getInt("semesterNo"),
+                null,
+                null
+        )));
+        return students;
     }
 
     public Curriculum initCurriculum() {
@@ -28,6 +43,15 @@ public class LabelingController extends Controller {
     }
 
     public ArrayList<Advisor> initAdvisors() {
-        return null;
+        advisors = new ArrayList<>();
+        ArrayList<JSONObject> objects = readJSONFiles("Students");
+        objects.forEach((n) -> advisors.add(new Advisor(
+                null,
+                n.getString("name"),
+                n.getString("surname"),
+                n.getString("ssn"),
+                n.getString("gender").charAt(0)
+        )));
+        return advisors;
     }
 }

86d8bfac8da09c82de91638a1e56ff6a0963f06b
erennbaspinar <ishakeren2001@gmail.com>
2022-11-13 18:01:29 +0300

    skeleton Person class added

---
+++ iteration1/Models/Person.java
@@ -0,0 +1,41 @@
+package iteration1.Models;
+
+public abstract class Person extends Model {
+    private String name;
+    private String surname;
+    private String ssn;
+    private Character gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+}
+

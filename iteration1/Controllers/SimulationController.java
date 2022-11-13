b1171b65e3a5e71bbb70c48479fef26ecb1ab272
YasinSefa15 <yasekirman@gmail.com>
2022-11-14 00:05:45 +0300

    SimulationController all students registers randomly to courses

--- iteration1/Controllers/SimulationController.java
+++ iteration1/Controllers/SimulationController.java
@@ -6,12 +6,55 @@ import iteration1.Models.Student;
 import iteration1.Models.Advisor;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class SimulationController extends Controller{
+public class SimulationController extends Controller {
+    ArrayList<Course> courses;
+    ArrayList<Student> students;
+    ArrayList<Advisor> advisors;
+    Curriculum curriculum;
+
     SimulationController(ArrayList<Course> courses, ArrayList<Student> students,
-                         Curriculum curriculum, ArrayList<Advisor> advisors){
-
+                         Curriculum curriculum, ArrayList<Advisor> advisors) {
+        this.courses = courses;
+        this.students = students;
+        this.curriculum = curriculum;
+        this.advisors = advisors;
     }
 
+    public void startSimulation() {
+        registerStudentsToClasses();
+    }
+
+    public void registerStudentsToClasses() {
+        StudentController studentController = new StudentController();
+        studentController.setError(this.getError());
+
+        Random randomizer = new Random();
+        Student student = students.get(randomizer.nextInt(students.size()));
+        Course course = null;
+
+        //todo : burası ile oynanır sonra.
+        while (student.getSelectedCourses().size() != 8) {
+            course = randomCourse(student);
+            studentController.registerToCourse(student, course);
+        }
+    }
+
+    public Course randomCourse(Student student) {
+        Random randomizer = new Random();
+        Course course = null;
+        boolean ableToRegister = false;
+
+        while (!ableToRegister) {
+            course = courses.get(randomizer.nextInt(courses.size()));
+            if (student.getTranscript().getCompletedCourses().contains(course)
+                    || student.getTranscript().getFailedCourses().contains(course)) {
+                ableToRegister = true;
+            }
+        }
+
+        return course;
+    }
 
 }

34d3b39195cd7cfcb46d03cc61500ee988b2678d
Tegralyn <tegralyn@gmail.com>
2022-11-14 00:12:32 +0300

    StudentController: Changing course status and registering

--- iteration1/Controllers/StudentController.java
+++ iteration1/Controllers/StudentController.java
@@ -2,15 +2,32 @@ package iteration1.Controllers;
 
 import iteration1.Models.Course;
 import iteration1.Models.Student;
-import iteration1.Models.Transcript;
 
-public class StudentController extends Controller{
+public class StudentController extends Controller {
 
-    public boolean registerToCourse(Student student, Course course){
+    public boolean registerToCourse(Student student, Course course) {
+        boolean permission = student.getAdvisor().courseAvailability(student, course);
+
+        if(!permission){
+            return false;
+        }
+
+        //advisordan geçtiyse
+        student.addToSelectedCourses(course, changeCourseStatus(student, course));
         return false;
     }
 
-    public boolean changeCourseStatus(Transcript transcript){
+    //todo : uml güncellenecek
+    //transkript e gerek var mı ? student ve course gelsin yeter? buradan güncelleriz
+    public boolean changeCourseStatus(Student student, Course course) {
+        if ((int) (Math.round(Math.random())) == 1) {
+            student.getTranscript().addToCompletedCourses(course);
+            student.getTranscript().setCompletedCredit(
+                    student.getTranscript().getCompletedCredit() + course.getCredit()
+            );
+            return true;
+        }
+        student.getTranscript().addToFailedCourses(course);
         return false;
     }
 }

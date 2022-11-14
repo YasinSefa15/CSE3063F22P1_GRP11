a33fbbb8e0639d67ae49988b5bfe12d5bf51a9ef
Hakan Uz <uz_hakan20@hotmail.com>
2022-11-14 19:41:47 +0300

    skeleton updated Mandatory Class constructor

--- iteration1/Models/Mandatory.java
+++ iteration1/Models/Mandatory.java
@@ -6,7 +6,20 @@ import java.util.ArrayList;
 public class Mandatory extends Course {
     private ArrayList<Lab> lab = new ArrayList<>();
 
-    public Mandatory(){}
+    public Mandatory(String name, String code, int credit, int requiredCredits, int quota, int semester,
+                     ArrayList<String> preRequisiteCourses, ArrayList<String> weeklyHours,
+                     ArrayList<Student> registeredStudents, ArrayList<Lab> lab){
+        this.setName(name);
+        this.setCode(code);
+        this.setCredit(credit);
+        this.setRequiredCredits(requiredCredits);
+        this.setQuota(quota);
+        this.setSemester(semester);
+        this.setPreRequisiteCourses(preRequisiteCourses);
+        this.setWeeklyHours(weeklyHours);
+        this.setRegisteredStudents(registeredStudents);
+        this.lab = lab;
+    }
     public void assignQuota(){
 
     }

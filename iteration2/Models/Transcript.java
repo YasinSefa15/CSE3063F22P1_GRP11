d001dc6302eba3a6c240f12d1065286ca9f90484
berkkanrencber <101739059+berkkanrencber@users.noreply.github.com>
2022-12-17 14:15:47 +0300

    // Transcript class logging process is done.

--- iteration2/Models/Transcript.java
+++ iteration2/Models/Transcript.java
@@ -1,10 +1,14 @@
 package iteration2.Models;
 
-import org.hamcrest.CoreMatchers;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Transcript extends Model{
     private double gpa;
@@ -12,11 +16,34 @@ public class Transcript extends Model{
     private ArrayList<Course> completedCourses;
     private  ArrayList<Course> failedCourses;
 
+    private static final String FILE_PATH="/iteration2/Logs/Transcript.log";
+    private static Logger logger=Logger.getLogger(Transcript.class.getName());
+    private static FileHandler fileHandler;
+
+    static {
+        try {
+            fileHandler = new FileHandler(System.getProperty("user.dir")+FILE_PATH,true);
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void customLog(boolean type,String message){
+        SimpleFormatter formatter =new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+        if(type)
+            logger.info(message);
+        else
+            logger.warning(message);
+    }
+
     public Transcript(double gpa, int completedCredit, ArrayList<Course> completedCourses, ArrayList<Course> failedCourses) {
         this.gpa = gpa;
         this.completedCredit = completedCredit;
         this.completedCourses = completedCourses;
         this.failedCourses = failedCourses;
+        customLog(true,"Transcript constructor method is called and new transcript object is generated.");
     }
 
     public void  calculateGPA(){
@@ -34,6 +61,7 @@ public class Transcript extends Model{
             tempGpa+=courseCredit*0;
         }
         this.gpa=tempGpa/totalCredit;
+        customLog(true,"The student's GPA is calculated.");
     }
 
     public double getGpa() {
@@ -52,7 +80,10 @@ public class Transcript extends Model{
 
     public void setCompletedCredit(int completedCredit) {
         if (completedCredit>=0){
+            //CustomLog.customLog(true,"The student's completed credit is set.");
             this.completedCredit = completedCredit;
+        }else {
+            //CustomLog.customLog(false,"The student's completed credit is not set because the entered completed credit is less than 0.");
         }
     }
 
@@ -87,12 +118,19 @@ public class Transcript extends Model{
     public void addToCompletedCourses(Course course){
         if (!completedCourses.contains(course)){
             completedCourses.add(course);
+            customLog(true,"A new course has been added to the courses that the student has completed.");
+        }else {
+            customLog(false,"A new course could not be added to the courses that the student completed" +
+                    " because it has already completed it.");
         }
     }
 
     public void addToFailedCourses(Course course){
         if (!failedCourses.contains(course)){
             failedCourses.add(course);
+            customLog(true,"A new course has been added to the courses that the student has failed.");
+        }else {
+            customLog(false,"A new course could not be added to the courses that the student failed because it has already failed it.");
         }
     }
 

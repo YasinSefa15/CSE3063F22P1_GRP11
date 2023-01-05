472876b4820165c89ea6ffdcfb8c20ff46b72fbf
@dogukansahin551 <dogukansahin551@gmail.com>
2023-01-05 19:59:59 +0300

    Functions were written to check quota semester and credit. Additionally, a function was written to identify courses completed

--- iteration3/Models/Advisor.py
+++ iteration3/Models/Advisor.py
@@ -4,5 +4,28 @@ class Advisor(Person):
         self._students = students
         self._error = error
 
+    def check_quota(self, course):
+        if course.get_registered_students_count() >= course.get_quota():
+            return False
+        return True
+
+    def checkSemester(self, student, course):
+        if course.getSemester() <= student.getSemesterNo():
+            return true
+        return false
+
+
+    def checkCredit(self, student, course):
+        if course.getRequiredCredits() > student.getTranscript().getCompletedCredit():
+            return true
+        return false
+
+    def setCompletedCourses(self, completedCourses):
+        for i in range(len(completedCourses)):
+            for j in range(len(self.completedCourses)):
+                if completedCourses[i].getCode() != self.completedCourses[j].getCode():
+                    self.completedCourses.append(completedCourses[i])
+
+
 
 

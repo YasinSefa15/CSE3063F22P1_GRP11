e7c9dd0f846c5dc67cfafc16f0a7d265febd409d
Hakan Uz <uz_hakan20@hotmail.com>
2023-01-09 00:36:19 +0300

    change variables to private in Elective, Lab and Mandatory classes

--- iteration3/Models/Elective.py
+++ iteration3/Models/Elective.py
@@ -1,14 +1,17 @@
+from iteration3.Models.Course import Course
+
+
 class Elective(Course):
     def __init__(self, name, code, credit, required_credits, quota, semester, pre_requisite_courses, weekly_hours, type):
-        self.name = name
-        self.code = code
-        self.credit = credit
-        self.required_credits = required_credits
-        self.quota = quota
-        self.semester = semester
-        self.pre_requisite_courses = pre_requisite_courses
-        self.weekly_hours = weekly_hours
-        self.type = type
+        self.__name = name
+        self.__code = code
+        self.__credit = credit
+        self.__required_credits = required_credits
+        self.__quota = quota
+        self.__semester = semester
+        self.__pre_requisite_courses = pre_requisite_courses
+        self.__weekly_hours = weekly_hours
+        self.__type = type
 
     def set_type(self, type):
         self.type = type

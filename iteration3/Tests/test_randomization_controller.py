9ebf71415374e4ffed4569e48d29fe8591a7ca1f
Emirhan Erdoğan <emrhnerdogan01@gmail.com>
2023-01-09 02:03:02 +0300

    Randomization controller test updated

--- iteration3/Tests/test_randomization_controller.py
+++ iteration3/Tests/test_randomization_controller.py
@@ -5,10 +5,22 @@ from iteration3.Controllers.RandomizationController import RandomizationControll
 
 
 class TestRandomizationController(unittest.TestCase):
-    def test_something(self):
-        ran_con = RandomizationController()
-        self.assertEqual(True, ran_con.choose_advisor())  # add assertion here
+    def setUp(self):
+        self.advisors = ['Çiğdem Eroğlu Erdem', 'Müjdat Soytürk']
+        self.curriculums = ['Digital Logic Design', 'Modeling and Discrete Simulation']
+        self.controller = RandomizationController(self.advisors, self.curriculums)
 
+    def test_generate_students(self):
+        students = self.controller.generate_students(5, 1)
+        self.assertEqual(len(students), 5)
+        for student in students:
+            self.assertIsInstance(student, Student)
+
+    def test_generate_transcript(self):
+        self.assertTrue(self.controller.generate_transcript())
+
+    def test_choose_advisor(self):
+        self.assertIn(self.controller.choose_advisor(), self.advisors)
 
 if __name__ == '__main__':
     unittest.main()

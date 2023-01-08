ea872c06bd8d136bb80965fda6d48ba8328e935d
@dogukansahin551 <dogukansahin551@gmail.com>
2023-01-08 23:22:41 +0300

    syntax errors are fixed

--- iteration3/Models/Advisor.py
+++ iteration3/Models/Advisor.py
@@ -16,60 +16,56 @@ class Advisor(Person):
     def course_availability(student, course):
         if not check_semester(student, course):
             student.add_error(error.report_error(1001, [course.name, str(course.semester), str(student.semester_no)]))
-            custom_log(False,
-                       "Checked semester of students and courses that requested by students and they are declined ")
-            return False
+            return false
         if not check_credit(student, course):
             student.add_error(error.report_error(1002, [course.name, str(student.transcript.completed_credit)]))
-            custom_log(False, "Compared credit of students and required credit of course and it is not enough.")
-            return False
+            return false
         if not check_quota(course):
             student.add_error(error.report_error(1003, [course.name]))
-            custom_log(False, "Check quota of course and there is not enough space for student.")
-            return False
+            return false
         temp_return = check_pre_requisite(student, course)
         if not temp_return[0]:
             student.add_error(error.report_error(1004, [course.name, temp_return[1]]))
-            return False
+            return false
         temp_return_2 = check_collision(student)
         if not temp_return_2[0]:
             student.add_error(error.report_error(1005, [temp_return_2[1], temp_return_2[2], temp_return_2[3]]))
-            return False
+            return false
         if not check_elective(student, course):
-            return False
+            return false
         if not fte_takeable(student, course):
             # return False
             pass
 
-        return True
+        return true
 
     def check_quota(self, course):
         if course.get_registered_students_count() >= course.get_quota():
-            return False
-        return True
+            return false
+        return true
 
-    def checkSemester(self, student, course):
+    def check_semester(self, student, course):
         if course.getSemester() <= student.getSemesterNo():
             return true
         return false
 
 
-    def checkCredit(self, student, course):
+    def check_credit(self, student, course):
         if course.getRequiredCredits() > student.getTranscript().getCompletedCredit():
             return true
         return false
 
     def check_pre_requisite(student, course):
         temp_completed_course = student.transcript.completed_courses
-        availability = True
+        availability = true
         error_info = [None, None]
         for i in range(len(course.pre_requisite_courses)):
             for j in range(len(temp_completed_course)):
                 if course.pre_requisite_courses[i].code != temp_completed_course[j].code:
-                    availability = False
+                    availability = false
                     error_info[1] = course.pre_requisite_courses[i].code
                 else:
-                    availability = True
+                    availability = true
                     error_info[1] = ""
                     break
             if not availability:
@@ -81,7 +77,7 @@ class Advisor(Person):
         courses_of_hash = student.selected_courses.keys()
         courses = list(courses_of_hash)
         error_info = [None, None, None, None]
-        error_info[0] = True
+        error_info[0] = true
         error_info[1] = ""
         error_info[2] = ""
         error_info[3] = ""
@@ -90,7 +86,7 @@ class Advisor(Person):
             for j in range(i + 1, len(courses)):
                 temp = [x for x in temp if x in courses[j].weekly_hours]
                 if len(temp) != 0:
-                    error_info[0] = False
+                    error_info[0] = false
                     error_info[1] = courses[i].name
                     error_info[2] = courses[j].name
                     error_info[3] = str(len(temp))
@@ -103,13 +99,13 @@ class Advisor(Person):
             if isinstance(k, Elective):
                 count += 1
         if count >= 2:
-            return False
-        return True
+            return false
+        return true
 
     def fte_takeable(student, course):
         if student.semester_no >= 7 and isinstance(course, Elective) and course.type == ElectiveType.FTE:
-            return True
-        return False
+            return true
+        return false
 
     def get_students(self):
         return self.students

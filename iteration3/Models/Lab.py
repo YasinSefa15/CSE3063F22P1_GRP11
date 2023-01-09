f75fb66503503048920e9a7578cbdd9efd35121b
Semihhasal <semih.hasal@htomail.com>
2023-01-09 03:08:37 +0300

    lab changing

--- iteration3/Models/Lab.py
+++ iteration3/Models/Lab.py
@@ -7,7 +7,9 @@ from iteration3.Models.Course import Course
 
 
 class Lab(Course):
-    def __init__(self, name: str, code: str, credit: int, required_credits: int, quota: int, semester: int, pre_requisite_courses: List[Course], weekly_hours: List[str]) -> None:
+    def __init__(self, name, code, credit, required_credits, quota, semester,
+                 pre_requisite_courses, weekly_hours):
+        super().__init__()
         self.__name = name
         self.__code = code
         self.__credit = credit
@@ -18,13 +20,13 @@ class Lab(Course):
         self.__weekly_hours = weekly_hours
         self.__section_number = self._generate_section_number()
 
-    def _generate_section_number(self) -> int:
-        return int(math.floor(random.random() * 4 + 1))
+    def _generate_section_number(self):
+        return math.floor(random.random() * 4 + 1)
 
     def get_credit(self):
         return self.__credit
 
-    def set_section_number(self, section_number: int) -> None:
+    def set_section_number(self, section_number: int):
         self.section_number = section_number
 
     def to_json(self) -> json:
